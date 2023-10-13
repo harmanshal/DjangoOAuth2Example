492d50e78dd1395863bc05cad7d3020b2beba9d0
Vovan <vovanex12@gmail.com>
2023-10-13 14:56:28 +0300

    Настройки провайдера теперь не нужны

--- client/main/settings/authentication.py
+++ client/main/settings/authentication.py
@@ -1,21 +1,11 @@
-import os
-
 LOGIN_URL = 'social/login/django/'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
-
-OAUTH2_PROVIDER = {
-    'RESOURCE_SERVER_INTROSPECTION_URL': 'http://provider-nginx:8000/oauth/introspect/',
-    'RESOURCE_SERVER_INTROSPECTION_CREDENTIALS': (
-        os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET')
-    )
-}
 
 AUTHENTICATION_BACKENDS = (
     "main.oauth_backend.DjangoOAuthToolkitBackend",
     "django.contrib.auth.backends.ModelBackend"
 )
-
 
 OAUTH2_BACKEND = {
     # todo: Проблемы со спец символами
