5665f74ca5d7c0b067c1deea8e107bebcc5114cf
Vovan <vovanex12@gmail.com>
2023-10-13 15:30:17 +0300

    Никаких проблем со спец символами не было, я просто не понимал как это работает

--- client/main/settings/authentication.py
+++ client/main/settings/authentication.py
@@ -8,9 +8,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 OAUTH2_BACKEND = {
-    # todo: Проблемы со спец символами
-    'CLIENT_ID': 'self',
-    'CLIENT_SECRET': 'self',
+    'CLIENT_ID': 'r3CuSXmOtHOGn0XWTy1MmmwzT3RI7I0Hu4i6LIqP',
+    'CLIENT_SECRET': 'client_secret&%$=',
     'AUTHORIZATION_URL': 'http://127.0.0.1:8000/oauth/authorize/',
     'ACCESS_TOKEN_URL': 'http://provider-nginx:8000/oauth/token/',
     'USER_DETAILS_URL': 'http://provider-nginx:8000/api/users/me/'
