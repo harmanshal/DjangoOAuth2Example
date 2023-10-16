0833c1c3e64313cbb14b4360425f339eb79b6784
Vovan <vovanex12@gmail.com>
2023-10-16 16:28:21 +0300

    Настройки провайдера нужны для авторизации в api

--- client/main/settings/authentication.py
+++ client/main/settings/authentication.py
@@ -1,3 +1,5 @@
+import os
+
 LOGIN_URL = 'social/login/django/'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
@@ -13,4 +15,11 @@ OAUTH2_BACKEND = {
     'AUTHORIZATION_URL': 'http://127.0.0.1:8000/oauth/authorize/',
     'ACCESS_TOKEN_URL': 'http://provider-nginx:8000/oauth/token/',
     'USER_DETAILS_URL': 'http://provider-nginx:8000/api/users/me/'
+}
+
+OAUTH2_PROVIDER = {
+    'RESOURCE_SERVER_INTROSPECTION_URL': 'http://provider-nginx:8000/oauth/introspect/',
+    'RESOURCE_SERVER_INTROSPECTION_CREDENTIALS': (
+        'r3CuSXmOtHOGn0XWTy1MmmwzT3RI7I0Hu4i6LIqP', 'client_secret&%$='
+    )
 }
